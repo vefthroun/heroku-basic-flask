6d1eae42b061b71b4497607323caded00a92de4c
gjg <gjg@tskoli.is>
2019-01-10 13:37:20 +0000

    time

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-#    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
     return """
     <h1>Hello heroku</h1>
     <p>It is currently {time}.</p>
 
     <img src="http://loremflickr.com/600/400" />
-    """ #.format(time=the_time)
+    """ .format(time=the_time)
 
 if __name__ == '__main__':
     app.run()
