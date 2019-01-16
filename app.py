35d4c87547eacee6a20f278b0eb5a15d0088e446
gjg <gjg@tskoli.is>
2019-01-16 12:06:12 +0000

    fleiri kisur

--- app.py
+++ app.py
@@ -5,16 +5,33 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+#    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
     return """
     <h1>Hello heroku</h1>
     <p>It is currently {time}.</p>
 
     <img src="http://loremflickr.com/600/400" />
-    """ .format(time=the_time)
+    <p><a href="/sida2" title="Síða 2">Síða 2 </a> | <a href="/sida3" title="Síða 3">Síða 3 </a>
+    """ #.format(time=the_time)
+
+@app.route('/sida2')
+def page2():
+    return """
+    <h1>Síða 2</h1>
+    <p>It is currently cathour.</p>
+    <img src="http://loremflickr.com/600/400" />
+    <p><a href="/" title="Forsíða">Forsíða </a> | <a href="/sida3" title="Síða 3">Síða 3 </a>
+    """ 
+@app.route('/sida3')
+def page3():
+    return """
+    <h1>Síða 3</h1>
+    <p>More cats.</p>
+    <img src="http://loremflickr.com/600/400" />
+    <p><a href="/" title="Forsíða">Forsíða </a> | <a href="/sida2" title="Síða 2">Síða 2 </a>
+    """ 
 
 if __name__ == '__main__':
-    app.run()
-    #app.run(debug=True, use_reloader=True)
-
+    #app.run()
+    app.run(debug=True, use_reloader=True)
