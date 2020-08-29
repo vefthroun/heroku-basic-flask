89871586d5e07f9f5d041947e81b9b26bda881db
gjg <gjg@tskoli.is>
2020-08-29 08:06:41 +0000

    index ok

--- app.py
+++ app.py
@@ -14,22 +14,6 @@ def homepage():
     <img src="http://loremflickr.com/600/400" />
     """ .format(time=the_time)
 
-@app.route('/sida2')
-def page2():
-    return """
-    <h1>Síða 2</h1>
-    <p><a href="/" title="Forsíða">Forsíða </a> | <a href="/sida3" title="Síða 3">Síða 3 </a></p>
-    <p>It is currently cathour.</p>
-    <img src="http://loremflickr.com/600/400" />
-    """ 
-@app.route('/sida3')
-def page3():
-    return """
-    <h1>Síða 3</h1>
-    <p><a href="/" title="Forsíða">Forsíða </a> | <a href="/sida2" title="Síða 2">Síða 2 </a></p>
-    <p>More cats.</p>
-    <img src="http://loremflickr.com/600/400" />
-    """ 
 
 if __name__ == '__main__':
     app.run()
